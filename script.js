f026032ce83cfbd2cddb2a4091fbb878c50e6e7f
VINICIUS LOPES DA SILVA <0200832311013>
2024-03-15 12:00:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const checkbox = document.querySelector('.check')
+// const button = document.querySelector('#slider-button')
+
+// const slide = () => {
+
+//   if(button.classList.contains('right')){
+//     button.style = ' background: linear-gradient(45deg, rgba(255,0,0,1) 0%, rgba(156,0,0,1) 100%);'
+//     button.classList.remove('right')
+//     button.classList.add('left')
+//   } else{
+//     button.style = 'background: linear-gradient(45deg, rgba(9,255,0,1) 0%, rgba(0,161,10,1) 100%);'
+//     button.classList.remove('left')
+//     button.classList.add('right')
+//   }
+// }
